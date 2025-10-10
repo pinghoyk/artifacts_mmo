59d95079f8bb78b93db6c69f916e8b47f87ca05d
pinghoyk <pinghoyk009@gmail.com>
2025-10-10 12:58:27 +0500

    добавила библиотеки

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"log"
+	"errors"
+	"log"
+	"os"
+	"io"
+	"strings"
+	"bytes"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+
 }
