e7bbf5d86011079f7cf4de7cad053c23daa1cb50
pinghoyk <pinghoyk009@gmail.com>
2025-10-10 13:10:37 +0500

    добавила структуры

--- main.go
+++ main.go
@@ -15,6 +15,27 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Character struct {
+	Name 		string 			`json:"name"`
+	Account 	string 			`json:"account"`
+	Skin 		string 			`json:"skin"`
+	Level 		int 			`json:"level"`
+	Xp 			int 			`json:"xp"`
+	Gold 		int 			`json:"gold"`
+	Hp 			int 			`json:"hp"`
+	Cooldown	int 			`json:"cooldown"`
+	X 			int 			`json:"x"`
+	Y 			int 			`json:"y"`
+	Inventory []InventoryItem 	`json:"inventory"`
+}
+
+type InventoryItem struct {
+	Slot 		int 	`json:"slot"`
+	Code 		string 	`json:"code"`
+	Quantity 	int 	`json:"quantity"`
+}
+
+
 func main() {
 
 }
