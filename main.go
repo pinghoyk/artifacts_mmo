d7879f38764bdebed1769602d9724004b9d6c4fe
pinghoyk <pinghoyk009@gmail.com>
2025-10-10 14:38:36 +0500

    Создание get запроса

--- main.go
+++ main.go
@@ -40,11 +40,71 @@ func mustGetEnv (key string) string {
 	return value
 }
 
+func createRequest(url, token string) (*http.Request, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	return req, nil
+}
+
+func getCharacter(client *http.Client, url, token string) (*Character, error) {
+    req, err := createRequest(url, token)
+    if err != nil {
+        return nil, err
+    }
+
+    resp, err := client.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return nil, fmt.Errorf("API вернуло ошибку: %s", resp.Status)
+    }
+
+    body, err := io.ReadAll(resp.Body)
+    fmt.Println("Сырой ответ сервера (получение персонажа):", string(body))
+    if err != nil {
+        return nil, err
+    }
+
+    var response struct {
+        Data Character `json:"data"`
+    }
+    if err := json.Unmarshal(body, &response); err != nil {
+        return nil, err
+    }
+
+    return &response.Data, nil
+}
+
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Ошибка загрузки файла .env")
 	}
+
+	token := mustGetEnv("API_TOKEN")
+	baseURL := mustGetEnv("URL")
+	characterName := mustGetEnv("CHARACTER_NAME")
+
+	characterURL := fmt.Sprintf("%s/characters/%s", strings.TrimSuffix(baseURL, "/"), characterName)
+
+	client := &http.Client{}
+	
+	character, err := getCharacter(client, characterURL, token)
+	if err != nil {
+	    log.Fatal("Ошибка получения персонажа:", err)
+	}
+
+	if character == nil {
+	    log.Fatal("Персонаж не найден")
+	}
+	
+	fmt.Printf("Текущая позиция: (%d, %d)\n", character.X, character.Y)
 
 }
