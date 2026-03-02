1c15885616ad9a141a5dde8379da183eaf8b2a31
pinghoyk <pinghoyk009@gmail.com>
2026-03-02 15:36:14 +0500

    функция для открывания .env файла через поиск go.mod

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,25 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/pinghoyk/artifacts_mmo/internal/bot"
 )
+
+func findProjectRoot() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
+			return dir
+		}
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			break
+		}
+		dir = parent
+	}
+	return "."
+}
+
 func main() {
 
 }
